bfc2a7cc689b3ec341043e9d2efd28019726f28e
Ktae23 <pktpkt8917@gmail.com>
2021-02-04 15:00:04 +0900

    Add new package

--- JAVA/Baisic_Ch6/src/test/constructor/A.java
+++ JAVA/Baisic_Ch6/src/test/constructor/A.java
@@ -1,28 +1,39 @@
 package test.constructor;
 
 public class A extends Object{
-	
+	private int i;
+	private String s;		
+	public int getI() {
+		return i;
+	}
+	public void setI(int i) {
+		this.i = i;
+	}
+	public String getS() {
+		return s;
+	}
+	public void setS(String s) {
+		this.s = s;
+	}
 	public A() {
 		super();
 		System.out.println("A() 생성자 호출");
-	}
-	
-	public A(int i) {
-		super();
-		System.out.println("A(int i) 생성자 호출");
-	}
-	
+	}	
+//	public A(int i) {
+//		super();
+//		System.out.println("A(int i) 생성자 호출");
+//	}	
 	public A(int i, String s) {
 		super();
+		setI(i);
+		setS(s);
 		System.out.println("A(int i, String s) 생성자 호출");
 	}
-
-	public static void main(String[] args) {
-		A a1 = new A();
-		A a2 = new A(10);
-		A a3 = new A(20,"java");
+	public static void main(String[] args) {		
+		A a3=new A(10,"java");
 		
 	}
-	
-	
+
+}
+
 }
