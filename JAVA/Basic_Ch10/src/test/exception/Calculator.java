fad50b53981de552a55edf72907e48f267a57d07
Ktae23 <pktpkt8917@gmail.com>
2021-02-05 17:34:46 +0900

    Add Exception Throw and Throws Practice

--- JAVA/Basic_Ch10/src/test/exception/Calculator.java
+++ JAVA/Basic_Ch10/src/test/exception/Calculator.java
@@ -2,7 +2,7 @@ package test.exception;
 
 public class Calculator {
 
-	public int divide(int x, int y) throws Exception {
+	public int divide(int x, int y) throws MyException {
 		int z=0;
 		if(y==0) {
 			throw new MyException("y값을 0으로 입력하지 마세요");
