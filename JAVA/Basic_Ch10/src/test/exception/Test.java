93bfcec035d706e70ff403d6bf8f905f38e9d9b0
Ktae23 <pktpkt8917@gmail.com>
2021-02-05 17:44:14 +0900

    Add Finally Exception Keword

--- JAVA/Basic_Ch10/src/test/exception/Test.java
+++ JAVA/Basic_Ch10/src/test/exception/Test.java
@@ -8,11 +8,14 @@ public class Test {
 		int result = 0;
 		try {
 			result = c.divide(100, 0);
+			c=null;
+			c.divide(50,2);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
-		}
+		}finally {
 		System.out.println(result);
 		System.out.println("아주 중요한일 시작..");
+		}
 	}
 	
 }
