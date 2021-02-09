4f62bac4ab1992071e051b07938ecf4931c207b8
Ktae23 <pktpkt8917@gmail.com>
2021-02-09 11:51:40 +0900

    Make InnerClass

--- JAVA_Practice/Java_Project/src/client/ClientUi.java
+++ JAVA_Practice/Java_Project/src/client/ClientUi.java
@@ -8,13 +8,16 @@ import java.awt.Frame;
 import java.awt.Panel;
 import java.awt.TextArea;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowListener;
 
-
+	
 public class ClientUi {
-
-	public static void main(String[] args) {
-		
+	TextArea ta;
+	TextField tf;
+	
+	public void onCreate() {
 		Frame f = new Frame("나의 채팅");
 		Panel p=new Panel();	
 		Panel p1=new Panel();
@@ -25,42 +28,18 @@ public class ClientUi {
 		Checkbox c2 = new Checkbox("동영상");
 		Checkbox c3 = new Checkbox("문자");
 		
-		TextField tf=new TextField(20);
-		TextArea ta=new TextArea();
+		tf=new TextField(20);
+		ta=new TextArea();
 
 		WindowListener fHandler=new MyFrameHandler();
 		f.addWindowListener(fHandler);		
 		
-		MyButtonHandler b1Handler=new MyButtonHandler(ta,tf);				
+		MyButtonHandler b1Handler=new MyButtonHandler();			
 		b1.addActionListener(b1Handler);
 		
-		MyButtonHandler tfHandler=new MyButtonHandler(ta,tf);		
+		MyButtonHandler tfHandler=new MyButtonHandler();			
 		tf.addActionListener(tfHandler);
 
-		
-		
-/*		Button b2 = new Button("전송2");
-		Button b3 = new Button("전송3");
-		Button b4 = new Button("전송4");
-		Button b5 = new Button("전송5");
-*/
-/*		GridLayout mgr=new GridLayout(2,3);
-		FlowLayout mgr=new FlowLayout();
-		f.setLayout(mgr);
-*/		
-/*		f.add(b1, BorderLayout.CENTER);
-		f.add(b2, BorderLayout.NORTH);
-		f.add(p, BorderLayout.SOUTH);
-		f.add(b4, BorderLayout.EAST);
-		f.add(b5, BorderLayout.WEST);*/
-		
-/*		f.add(p, BorderLayout.SOUTH)
-		p.add(b1);
-		p.add(b2);
-		p.add(b3);
-		p.add(b4);
-		p.add(b5);*/
-		
 		
 		f.add(ta, BorderLayout.CENTER);
 		f.add(p, BorderLayout.SOUTH);
@@ -85,6 +64,26 @@ public class ClientUi {
 		f.setLocation(800, 200);
 		f.setSize(400,500);
 		f.setVisible(true);
+	}
 
+    // Main 메서드
+	public static void main(String[] args) {
+        //ClientUi 클래스 생성 및 메서드 호출
+		ClientUi ui=new ClientUi();
+		ui.onCreate();
+		
+
+	}
+	//ClientUi 클래스 내부에 선언한 클래스, ClientUi 내부에서 사용할 목적.
+	public class MyButtonHandler implements ActionListener{
+	
+			
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//하고자 하는 일
+			String msg=tf.getText();
+			ta.append(msg+"\n");
+			tf.setText("");
+		}
 	}
 }
