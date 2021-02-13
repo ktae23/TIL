607076c3cf18ae272423523f4d0ea0f8df09854d
Ktae23 <pktpkt8917@gmail.com>
2021-02-13 22:12:02 +0900

    Fix Compile Error

--- JAVA_Practice/Java_Project/src/client/ClientUi.java
+++ JAVA_Practice/Java_Project/src/client/ClientUi.java
@@ -1,7 +1,9 @@
 package client;
 
+import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Checkbox;
+import java.awt.Color;
 import java.awt.FileDialog;
 import java.awt.Frame;
 import java.awt.Menu;
@@ -12,6 +14,8 @@ import java.awt.TextArea;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
