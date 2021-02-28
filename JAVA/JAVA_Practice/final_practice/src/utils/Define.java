6d256bcaa605f9f1a423e83f018ebf9642c94f01
ktae23 <pktpkt8917@gmail.com>
2021-02-28 19:34:25 +0900

    Add Final Practice Do it 자바프로그래밍 입문 마지막 실습 과제 진행

---
+++ JAVA/JAVA_Practice/final_practice/src/utils/Define.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Define {
+
+	public static final int KOREAN = 1001;
+	public static final int MATH = 2001;
+	
+	public static final int AB_TYPE = 0;
+	public static final int SAB_TYPE = 1;
+}
