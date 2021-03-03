0e3c8b43769b3c6face6d4b431b085102c25c7f1
Ktae23 <pktpkt8917@gmail.com>
2021-03-03 20:25:44 +0900

    feat : Add today's practices

---
+++ JAVA/JAVA_Practice/Web1/src/web/controller/MainServlet.java
@@ -0,0 +1,22 @@
+package web.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Main")
+public class MainServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
