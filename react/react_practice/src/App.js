cd77efb79b85353cb3a236858c8fefd6c6e4c545
ktae23 <pktpkt8917@gmail.com>
2021-05-14 16:40:38 +0900

    DOCS : ADd react_component

--- react/react_practice/src/App.js
+++ react/react_practice/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import TOC from './componet/TOC';
+import Subject from './componet/Subject';
+import Content from './componet/Content';
+ 
 
 class App extends Component {
-render(){
-  return (
-    <div className="App">
-      Hello, React!!!
-    </div>
-  );
-}
+  render(){ 
+    return (
+      <div className="App">
+        Hello, React!!!
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react/react_practice/src/componet/Subject.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Subject extends Component {
+    // function render(){} 여야 하지만 javascript의 최신 스펙이 들어간 class 내부에서는 생략
+      render() {
+        return(
+          // 컴포넌트를 만들 때는 반드시 하나의 최상위 태그만 사용해야만 함
+          <header>
+            <h1>{this.props.title}</h1>
+            {this.props.sub}
+          </header>
+        );
+      }
+    }
+
+export default Subject;
