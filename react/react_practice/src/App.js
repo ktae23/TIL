8ca42acbd1dc407594e14b68a39516d9b6c20fef
ktae23 <pktpkt8917@gmail.com>
2021-05-14 17:26:14 +0900

    DPCS : ADd react_props&state

--- react/react_practice/src/App.js
+++ react/react_practice/src/App.js
@@ -6,12 +6,25 @@ import Content from './componet/Content';
  
 
 class App extends Component {
-  render(){ 
+  constructor(props){
+    super(props);
+    this.state={
+        subject:{title:"WEB",sub:"World Wide Web!"},
+        contents:[
+            {id:1, title:"HTML", desc:"HTML is for information."},
+            {id:2, title:"CSS", desc:"CSS is for design"},
+            {id:3, title:"JavaScript", desc:"JavaScript is for interactive"}
+        ]
+    }
+  }
+  render(){
     return (
       <div className="App">
         Hello, React!!!
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
+        <Subject 
+            title={this.state.subject.title} 
+            sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
--- react/react_practice/src/componet/TOC.js
+++ react/react_practice/src/componet/TOC.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 
 class TOC extends Component {
-    render() {
-        return(
-          <nav>
-              <ul>
-                  <li><a href="1.html">HTML</a></li>
-                  <li><a href="2.html">CSS</a></li>
-                  <li><a href="3.html">JavaScript</a></li>
-              </ul>
-          </nav>
-        );
-      }
+
+render() {
+    var lists=[];
+    var data = this.props.data;
+    var i = 0;
+    while(i<data.length){
+        lists.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>);
+        i = i+1;
+}
+    return(
+        <nav>
+            <ul>
+                {lists}
+            </ul>
+        </nav>
+    );
     }
+}
 
     export default TOC;
