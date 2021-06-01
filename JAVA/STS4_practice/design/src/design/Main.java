6b013ced456d329d77a49fd2c6569da2c725b8d6
ktae23 <pktpkt8917@gmail.com>
2021-06-01 18:15:49 +0900

    TIL : Design pattern

--- JAVA/STS4_practice/design/src/design/Main.java
+++ JAVA/STS4_practice/design/src/design/Main.java
@@ -1,47 +1,37 @@
 package design;
 
-import design.adapter.AirConditioner;
-import design.adapter.Cleanner;
-import design.adapter.Electronic110V;
-import design.adapter.HairDryer;
-import design.adapter.SocketAdapter;
-import design.singlton.Aclass;
-import design.singlton.Bclass;
-import design.singlton.SocketClient;
+import java.util.concurrent.atomic.AtomicLong;
+
+import design.aop.AopBrowser;
+import design.proxy.IBroweser;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		/*
-		//싱글톤
-		Aclass aclass = new Aclass();
-		Bclass bclass = new Bclass();
 		
-		SocketClient aClient = aclass.getSocketClient();
-		SocketClient bClient = bclass.getSocketClient();
 		
-		System.out.println("두 객체가 동일한가?");
-		System.out.println(aClient.equals(bClient));
-		*/
+	AtomicLong start = new AtomicLong();
+	AtomicLong end = new AtomicLong(); 
 		
-		HairDryer hairDryer = new HairDryer();
-		connect(hairDryer);
-		
-		Cleanner cleanner = new Cleanner();
-//		connect(cleanner);
-		
-		Electronic110V adapter = new SocketAdapter(cleanner);
-		connect(adapter);
-		
-		AirConditioner airConditioner = new AirConditioner();
-		Electronic110V airAdapter = new SocketAdapter(airConditioner);
-		connect(airAdapter);
-
-	}
+	IBroweser aopBrowser = new AopBrowser("www.naver.com", 
+			() -> {
+				System.out.println("before");
+				start.set(System.currentTimeMillis());
+			},
+			() -> {
+				long now = System.currentTimeMillis();
+				end.set(now - start.get());
+			}
+		);
+	aopBrowser.show();
+	System.out.println("loading time : " + end.get());
 	
-	//어댑터
-	public static void connect(Electronic110V electronic110v) {
-		electronic110v.powerOn();
+	aopBrowser.show();
+	System.out.println("loading time : " + end.get());
+	
+	
+	
 	}
-
 }
+	
