beb9833f74b79db93e256615b50c1e2353438d5f
ktae23 <pktpkt8917@gmail.com>
2021-06-18 09:29:17 +0900

    DOCS : add junit5 basic

--- JAVA/STS4_practice/jwp-2nd-onion/src/test/Calculator.java
+++ JAVA/STS4_practice/jwp-2nd-onion/src/test/Calculator.java
@@ -6,11 +6,11 @@ public class Calculator {
 		return i+j;
 	}
 	
-	int substract(int i, int j) {
+	int subtract(int i, int j) {
 		return i - j;
 	}
 	
-	int mulutiply(int i, int j) {
+	int multiply(int i, int j) {
 		return i * j;
 	}
 	int divide(int i, int j) {
@@ -20,8 +20,8 @@ public class Calculator {
 	public static void main(String[] args) {
 		Calculator cal = new Calculator();
 		System.out.println(cal.add(3,4));
-		System.out.println(cal.substract(5, 4));
-		System.out.println(cal.mulutiply(2, 6));
+		System.out.println(cal.subtract(5, 4));
+		System.out.println(cal.multiply(2, 6));
 		System.out.println(cal.divide(8, 4));
 	}
 }
