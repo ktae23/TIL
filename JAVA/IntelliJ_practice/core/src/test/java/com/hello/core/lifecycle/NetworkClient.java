a50279a34d8e905846df80a45f5b126485b2bf21
ktae23 <pktpkt8917@gmail.com>
2022-01-29 12:34:38 +0900

    TIL : spring basic principle 1. 빈 생명주기 - 인터페이스를 활용한 방식 (권장하지 않음)

---
+++ JAVA/IntelliJ_practice/core/src/test/java/com/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,46 @@
+package com.hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+    private  String url;
+
+    public NetworkClient(){
+        System.out.println("생성자 호출, url = " + url);
+
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작 시 호출
+    public void connect(){
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect(){
+        System.out.println("close: " + url);
+    }
+
+    // 의존 관계 주입 끝나면 호출
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 스프링 빈이 종료 될 때 호출
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+       disconnect(); 
+    }
+}
