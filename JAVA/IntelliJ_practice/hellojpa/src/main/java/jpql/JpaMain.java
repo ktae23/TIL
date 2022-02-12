5cccd6dbaca86be0f3fec4bc4ca877ae145e6d7d
ktae23 <pktpkt8917@gmail.com>
2022-02-13 01:00:55 +0900

    TIL : jpql basic 1. jpql 조회 방법 (페이징)

--- JAVA/IntelliJ_practice/hellojpa/src/main/java/jpql/JpaMain.java
+++ JAVA/IntelliJ_practice/hellojpa/src/main/java/jpql/JpaMain.java
@@ -18,54 +18,25 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-
-            em.persist(member);
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
             em.flush();
             em.clear();
 
-//            List<Member> members = em.createQuery("select m.team from Member m", Member.class)
-//                    .getResultList();
-//
-//            // join이 나간다고 예측하기 좋도록 명시하는것 추천
-//            List<Team> team = em.createQuery("select m.team from Member m join m.team t", Team.class)
-//                    .getResultList();
-//
-//            List<Address> resultList = em.createQuery("select o.address from Order o", Address.class)
-//                    .getResultList();
-
-
-//            // 스칼라 타입 조회 1 (Object 배열 반환)
-//            List resultList1 = em.createQuery("select distinct m.username, m.age from Member m")
-//                    .getResultList();
-//            Object o = resultList1.get(0);
-//            Object[] result = (Object[]) o;
-//            System.out.println("username = " + result[0]);
-//            System.out.println("age = " + result[1]);
-
-//            // 스칼라 타입 조회 2 (제네릭에 Object 배열 선언)
-//            List<Object[]> resultList2 = em.createQuery("select distinct m.username, m.age from Member m")
-//                    .getResultList();
-//            Object[] result = resultList2.get(0);
-//            System.out.println("username = " + result[0]);
-//            System.out.println("age = " + result[1]);
-
-            // 스칼라 타입 조회 3 (dto로 바로 조회)
-            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+            List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
                     .getResultList();
-            MemberDTO memberDTO = result.get(0);
-            System.out.println("username = " + memberDTO.getUsername());
-            System.out.println("age = " + memberDTO.getAge());
 
-
-
-//            Member member1 = em.createQuery("select m from Member m where m.username = :username", Member.class)
-//                    .setParameter("username", "member1")
-//                    .getSingleResult();
-
+            System.out.println("resultList.size() = " + resultList.size());
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
