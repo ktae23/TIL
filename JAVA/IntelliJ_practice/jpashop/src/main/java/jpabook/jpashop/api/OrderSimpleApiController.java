8129ce561fa9a5e5b48906a94020ab703a9e3049
ktae23 <pktpkt8917@gmail.com>
2022-02-18 17:39:07 +0900

    TIL : spring data jpa 1.스프링 데이터 jpa 공부

--- JAVA/IntelliJ_practice/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ JAVA/IntelliJ_practice/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,7 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
-import jpabook.jpashop.repository.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,7 @@ import static java.util.stream.Collectors.toList;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
@@ -47,9 +49,9 @@ public class OrderSimpleApiController {
                 .collect(toList());
     }
 
-    @GetMapping("/api/v4/simple-orders")
+    @GetMapping("/api/v4/orders")
     public List<OrderSimpleQueryDto> orderV4() {
-        return orderRepository.findOrderDtos();
+        return orderSimpleQueryRepository.findOrderDtos();
     }
 
     @Data
--- JAVA/IntelliJ_practice/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ JAVA/IntelliJ_practice/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -41,12 +41,25 @@ public class OrderRepository {
                 .getResultList();
     }
 
-    public List<OrderSimpleQueryDto> findOrderDtos() {
+
+
+    public List<Order> finAllWithItem() {
         return em.createQuery(
-                "select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d. address)" +
-                        " from Order o" +
-                        " join o.member m" +
-                        " join o.delivery d", OrderSimpleQueryDto.class
-        ).getResultList();
+                "select distinct o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                        "select distinct o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
     }
 }
