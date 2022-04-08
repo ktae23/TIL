0025cebfac48d3d885f60b1bca629799b1f6d857
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2022-04-08 19:28:47 +0900

    spring_mvc

--- JPA/data-jpa/src/main/java/study/datajpa/repository/MemberProjections.java
+++
@@ -1,8 +0,0 @@
-package study.datajpa.repository;
-
-public interface MemberProjections {
-
-    Long getId();
-    String getUsername();
-    String getTeamName();
-}
--- SPRING/core/src/main/java/com/hello/core/order/OrderService.java
+++
@@ -1,5 +0,0 @@
-package com.hello.core.order;
-
-public interface OrderService {
-    Order createOrder(Long memberId, String itemName, int itemPrice);
-}
