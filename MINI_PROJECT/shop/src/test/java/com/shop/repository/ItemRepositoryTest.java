e4fee770f8260355a7fe84647c0defd0dca5366a
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2022-04-09 00:10:53 +0900

    TEST : Add test with test config yaml 1. 테스트용 야믈을 주입 받아 스프링 부트 테스트 실행

---
+++ MINI_PROJECT/shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.shop.repository;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(args = "--spring.profiles.active=test")
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest() {
+        Item item = Item.builder()
+                .itemName("테스트 상품")
+                .price(10000)
+                .stockNumber(100)
+                .itemDetail("테스트 상품 상세 설명")
+                .itemSellStatus(ItemSellStatus.SELL)
+                .regTime(LocalDateTime.now())
+                .updateTime(LocalDateTime.now())
+                .build();
+
+        Item savedItem = itemRepository.save(item);
+        assertThat(item).isEqualTo(savedItem);
+    }
+}
