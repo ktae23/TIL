96d4b88ba0ff3fe8a0173e0e92ba340f6394b0f0
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2022-04-10 02:18:26 +0900

    mini project

---
+++ monaco-editor/src/main/java/com/example/monacoeditor/MonacoEditorApplication.java
@@ -0,0 +1,13 @@
+package com.example.monacoeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonacoEditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonacoEditorApplication.class, args);
+    }
+
+}
---
+++ todo-server/src/test/java/org/example/service/TodoServiceTest.java
@@ -0,0 +1,77 @@
+package org.example.service;
+
+import org.example.model.TodoModel;
+import org.example.model.TodoRequest;
+import org.example.repository.TodoRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TodoServiceTest {
+
+    @Mock
+    private TodoRepository todoRepository;
+
+    @InjectMocks
+    private TodoService todoService;
+
+    @Test
+    void add() {
+        when(this.todoRepository.save(any(TodoModel.class)))
+                .then(AdditionalAnswers.returnsFirstArg());
+
+        TodoRequest expected = new TodoRequest();
+        expected.setTitle("Test Title");
+
+        TodoModel actual = this.todoService.add(expected);
+
+        assertEquals(expected.getTitle(), actual.getTitle());
+    }
+
+    @Test
+    void searchById() {
+        TodoModel entity = new TodoModel();
+        entity.setId(123L);
+        entity.setTitle("Title");
+        entity.setOrder(0L);
+        entity.setCompleted(false);
+        Optional<TodoModel> optional = Optional.of(entity);
+
+
+        given(this.todoRepository.findById(anyLong()))
+                .willReturn(optional);
+
+        TodoModel actual = this.todoService.searchById(123L);
+
+        TodoModel expected = optional.get();
+
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getOrder(), actual.getOrder());
+        assertEquals(expected.getCompleted(), actual.getCompleted());
+        assertEquals(expected.getTitle(), actual.getTitle());
+
+    }
+
+    @Test
+    public void searchByIdFailed() {
+        given(this.todoRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+        assertThrows(ResponseStatusException.class, () -> {
+            this.todoService.searchById(123L);
+        });
+    }
+}
