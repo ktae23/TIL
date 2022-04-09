ef3eaacd115ba3df89329aa6452781b9a20f7a42
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2022-04-10 08:27:30 +0900

    TEST : Add findCartAndMember test 1. 장바구니 회원 엔티티 매핑 조회 테스트 추가

---
+++ shop/src/main/java/com/shop/entity/Cart.java
@@ -0,0 +1,31 @@
+package com.shop.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+}
