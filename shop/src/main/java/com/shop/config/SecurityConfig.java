b542b74531a113de738a824f930a5159d344a9a1
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2022-04-10 03:50:06 +0900

    Config : Add configs for mvc matcher & exception handling 1. 권한 기반 요청 처리 및 예외 핸들링 추가

--- shop/src/main/java/com/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,25 +27,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final MemberService memberService;
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .formLogin()
+        http.formLogin()
                 .loginPage("/members/login")
                 .defaultSuccessUrl("/")
                 .usernameParameter("email")
                 .failureUrl("/members/login/error")
                 .and()
                 .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher(("/members/logout")))
+                .logoutRequestMatcher(
+                        new AntPathRequestMatcher(("/members/logout")))
                 .logoutSuccessUrl("/")
                 .and()
                 .csrf()
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
                 .cors().configurationSource(corsConfigurationSource())
-                .and()
-                .authorizeRequests()
-                .anyRequest().permitAll();
+        ;
+
+        http.authorizeRequests()
+                .mvcMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+                .mvcMatchers("/admin/**").hasAuthority("ADMIN")
+                .anyRequest().permitAll()
+        ;
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(
+                        new CustomAuthenticationEntryPoint())
+        ;
     }
 
     @Override
