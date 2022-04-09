2654218dba364787df50fe7f7b7e7d5cc1d232a2
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2022-04-10 01:42:31 +0900

    FEAT : Add userDetailsService 1. 시큐리티에서 회원 정보 처리를 위한 기본 메서드 구현

--- MINI_PROJECT/shop/src/main/java/com/shop/service/MemberService.java
+++ MINI_PROJECT/shop/src/main/java/com/shop/service/MemberService.java
@@ -3,6 +3,10 @@ package com.shop.service;
 import com.shop.entity.Member;
 import com.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,7 +16,7 @@ import java.util.Optional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
@@ -26,5 +30,19 @@ public class MemberService {
         if (findMember.isPresent()) {
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Member> findMember = memberRepository.findByEmail(email);
+        if (findMember.isPresent()) {
+            Member member = findMember.get();
+            return User.builder()
+                    .username(member.getEmail())
+                    .password(member.getPassword())
+                    .roles(member.getRole().name())
+                    .build();
+        }
+        throw new UsernameNotFoundException(email);
     }
 }
