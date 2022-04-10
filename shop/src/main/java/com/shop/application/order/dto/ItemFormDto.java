621d9836c53fc77770f6ad56d75f1ccb7190509a
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2022-04-10 23:39:21 +0900

    FIX : Add Setter for ModelAttribute 1. 객체 매핑을 위한 세터 추가

--- shop/src/main/java/com/shop/application/order/dto/ItemFormDto.java
+++ shop/src/main/java/com/shop/application/order/dto/ItemFormDto.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
