231828dde5ee48d8678c7fd0ac202726f2300303
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2022-04-10 09:43:39 +0900

    FIX : it is not recommended from spring 1. This is not recommended -- please use permitAll via HttpSecurity#authorizeHttpRequests instead.

--- shop/src/main/java/com/shop/infrastructure/config/SecurityConfig.java
+++ shop/src/main/java/com/shop/infrastructure/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,11 +24,6 @@ import java.util.List;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final MemberService memberService;
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -51,6 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         ;
 
         http.authorizeRequests()
+                .antMatchers("/css/**", "/js/**", "/img/**").permitAll()
                 .mvcMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
                 .mvcMatchers("/admin/**").hasAuthority("ADMIN")
                 .anyRequest().permitAll()
