83b9df1d51e3b19dbfa37afceffe76e42d392d1a
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2022-04-11 23:52:47 +0900

    FIX : fix vcs error

--- MINI_PROJECT/shop/src/main/java/com/shop/ShopApplication.java
+++ MINI_PROJECT/shop/src/main/java/com/shop/ShopApplication.java
@@ -1,13 +1,13 @@
-package hello.aop;
+package com.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AopApplication {
+public class ShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AopApplication.class, args);
+		SpringApplication.run(ShopApplication.class, args);
 	}
 
 }
--- MINI_PROJECT/shop/src/main/java/com/shop/application/order/ItemService.java
+++ MINI_PROJECT/shop/src/main/java/com/shop/application/order/ItemService.java
@@ -71,4 +71,5 @@ public class ItemService {
         return item.getId();
     }
 
+
 }
