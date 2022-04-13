d66157003a20dda7dbf4c301b438984c228ee5a6
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2022-04-13 21:35:55 +0900

    TEST : Add order test 1. 주문 테스트 추가

---
+++ MINI_PROJECT/shop/src/test/java/com/shop/application/order/OrderServiceTest.java
@@ -0,0 +1,73 @@
+package com.shop.application.order;
+
+import com.shop.application.order.dto.OrderDto;
+import com.shop.domain.member.model.Member;
+import com.shop.domain.member.repository.MemberRepository;
+import com.shop.domain.order.model.Item;
+import com.shop.domain.order.model.Order;
+import com.shop.domain.order.model.OrderItem;
+import com.shop.domain.order.repository.ItemRepository;
+import com.shop.domain.order.repository.OrderRepository;
+import com.shop.infrastructure.constant.order.ItemSellStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(args = "--spring.profiles.active=test")
+@Transactional
+class OrderServiceTest {
+
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private ItemRepository itemRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public Item saveItem() {
+        Item item = Item.builder()
+                .itemName("테스트 상품")
+                .price(10000)
+                .itemDetail("테스트 상품 상세 설명")
+                .itemSellStatus(ItemSellStatus.SELL)
+                .stockNumber(100)
+                .build();
+
+        return itemRepository.save(item);
+    }
+
+    public Member saveMember() {
+        Member member = Member.builder()
+                .email("test@test.com")
+                .build();
+        return memberRepository.save(member);
+    }
+    @Test
+    @DisplayName("주문 테스트")
+    void order () throws Exception {
+        // given
+        final Item item = saveItem();
+        final Member member = saveMember();
+        final OrderDto orderDto = OrderDto.builder().count(10).id(item.getId()).build();
+
+        // when
+        final Long orderId = orderService.order(orderDto, member.getEmail());
+        final Order order = orderRepository.findById(orderId).orElseThrow(EntityNotFoundException::new);
+        final List<OrderItem> orderItems = order.getOrderItems();
+        final int totalPrice = orderDto.getCount() * item.getPrice();
+
+        // then
+        assertThat(totalPrice).isEqualTo(order.getTotalPrice());
+    }
+
+}
